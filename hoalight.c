cc40ceebbbc22669bd302422e5afd1e4e80705ef
Chikashi Miyama <chikashimiyama@gmail.com>
2021-03-22 15:43:36 +0100

    add IO

--- hoalight.c
+++ hoalight.c
@@ -11,6 +11,9 @@ static t_class *hoalight_class;
  
 typedef struct _hoalight {  
     t_object x_obj;
+    t_float azimuth;
+    t_float elevation;
+    t_float spread;
     t_outlet *a_out;
 } t_hoalight;  
  
@@ -19,16 +22,22 @@ void hoalight_bang(t_hoalight *x) {
 
     t_atom output[3];
     
-    SETFLOAT(&output[0], 1.0f);
-    SETFLOAT(&output[1], 2.0f);
-    SETFLOAT(&output[2], 3.0f);
+    SETFLOAT(&output[0], x->azimuth);
+    SETFLOAT(&output[1], x->elevation);
+    SETFLOAT(&output[2], x->spread);
 
     outlet_list(x->a_out, &s_list, 3, &output[0]);
 
 }  
  
+
 void *hoalight_new(void) {  
     t_hoalight *x = (t_hoalight *)pd_new(hoalight_class);  
+
+	floatinlet_new(&x->x_obj, &x->azimuth);
+	floatinlet_new(&x->x_obj, &x->elevation);
+	floatinlet_new(&x->x_obj, &x->spread);
+
     x->a_out = outlet_new(&x->x_obj, &s_list);
 
     return (void *)x;  
