52461036feaa5455bf38dc8fca74fed76f55ad45
Chikashi Miyama <chikashimiyama@gmail.com>
2021-03-29 22:02:12 +0200

    remove clamping

--- src/Blauert.cpp
+++ src/Blauert.cpp
@@ -7,14 +7,12 @@ namespace Blauert
 
     float toPhi(float azimuth)
     {
-        auto clamped = std::clamp(azimuth, -180.f, 180.f);
-        auto folded = clamped > 90.f ? clamped - 360.f : clamped;
+        auto folded = azimuth > 90.f ? azimuth - 360.f : azimuth;
         return (1.0f - (folded + 270.f) / 360.f) * pi * 2.f;
     }
 
     float toTheta(float elevation)
     {
-        auto clamped = std::clamp(elevation, -90.f, 90.f);
-        return clamped/180.f * pi;
+        return elevation/180.f * pi;
     }
 }
